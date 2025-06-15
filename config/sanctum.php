ee11636f60e9acbbc24a6d9a41b79d4898df802d
bernadetaindraa <220711678@students.uajy.ac.id>
2025-06-15 11:32:21 +0700

    cek

--- config/sanctum.php
+++ config/sanctum.php
@@ -35,7 +35,7 @@ return [
     */
 
     'guard' => ['web'],
-    'guard' => ['api_pegawai', 'api_pembeli', 'api_penitip', 'api_organisasi'],
+    // 'guard' => ['api_pegawai', 'api_pembeli', 'api_penitip', 'api_organisasi'],
 
     /*
     |--------------------------------------------------------------------------
